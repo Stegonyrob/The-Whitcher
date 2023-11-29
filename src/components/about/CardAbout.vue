6d8de75d0a02f7f7a045de4d651a851dd7750699
Stella Maris González Robert <stegonyrob@yahoo.es>
2023-11-29 12:58:45 +0100

    about finished

--- src/components/about/CardAbout.vue
+++ src/components/about/CardAbout.vue
@@ -1,55 +1,44 @@
 <script setup>
-
 import { useAboutStore } from '@/stores/aboutStore';
 
 const aboutStore = useAboutStore();
 const about = aboutStore.about;
 </script>
-<template> <div class="card bg-transparent" >
-    <div class="about-card   ">
-        <div v-for="(about, index) in about" :key="index" >
-                 <img :src="about.image" class="card-img rounded-start" :alt="about.name">
-                 <div class="card-body">
-                    <h2 class="card-name">{{ about.name }} </h2>
-                     <p class="card-bio">{{ about.bio }}</p>
-                    </div>
-    </div>
-              
+<template>
+    <div class="container  align-content-start me-5">
+      <div class="card ps-4 bg-transparent mb-3">
+        <div class="row g-0 mt-2">
+          <div class="col-3 mt-5">
+            <div v-for="(about, index) in about" :key="index">
+              <img :src="about.image" class="card-img col-sm-3 mt-1 rounded" :alt="about.name" style="object-fit: cover;">
             </div>
-   
-    </div>
-</template>
-<style>
-.card{
-    width: 80%;
-    height: auto;
-}
-.about-card{
-    display: inline-flex;
-    margin-left: 18rem;
-    width: 100%;
-    height: 40%;
-   
-}
-.card-name{
-    margin-top: 2rem;
-    margin-left: 55%;
-    margin-bottom: 2rem;
-    color: azure;
-   
-}
-.card-bio{
-    text-align: justify;
-    margin-left: 30rem;
-    margin-right: 2%;
-    width: 50%;
-    color: azure;
-}
-.card-img{
-margin-top: 3rem;
-height: 30% ;
-width: auto;
-margin-left: 40rem;
-}
-
- </style>
+          </div>
+          <div class="col-md-6">
+            <div v-for="(about, index) in about" :key="index">
+              <div class="card-body" style="text-align: justify;">
+                <h2 class="card-name ms-5 mt-5 mb-3">{{ about.name }} </h2>
+                <p class="card-bio ms-2 mt-5">{{ about.bio }}</p>
+              </div>
+            </div>
+          </div>
+        </div>      
+      </div>    
+    </div>      
+  </template>
+  
+  <style scoped lang="scss">
+  .container {
+    margin-top: -40rem;
+    margin-left: 20rem;
+    width: 90%;
+    z-index: 5;
+    .card-body{
+        color: azure;
+    }
+    .card-bio {
+    
+        margin-left: 5rem !important;
+    }
+  }
+  </style>
+  
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,39 +1,39 @@
 <script setup>
+
 import CardAbout from '../components/about/CardAbout.vue';
 </script>
 <template>
-  <div class="video-background">
-    <video autoplay loop muted>
-      <source src="../assets/img/the-witcher-3-white-wolf-desktop-wallpaperwaifu-com.mp4" type="video/mp4">
-    </video>
+    <div class="video-background">
+    <div class="video-wrapper">
+      <video autoplay loop muted>
+        <source src="../assets/img/the-witcher-3-white-wolf-desktop-wallpaperwaifu-com.mp4" type="video/mp4">
+      </video>
+    
     
     <CardAbout />
   </div>
-
+</div>
  </template>
 
-<style>
-
- 
+<style lang="scss">
 .video-background {
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   overflow: hidden;
 }
 
-.video-background video {
+.video-wrapper {
   position: absolute;
-  top: 50%;
-  left: 50%;
-  min-width: 100%;
-  min-height: 100%;
-  width: auto;
-  height: auto;
-  z-index: -1;
-  transform: translateX(-55%) translateY(-55%);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
-
-
+.video-wrapper video {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
 </style>
