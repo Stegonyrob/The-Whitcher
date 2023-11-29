4a0e83b39420a53a87c5936e2209f6be17bc1d96
Colunga-D <xkould@gmail.com>
2023-11-29 10:28:00 +0100

    add page news

--- src/components/NewsContainer.vue
+++ src/components/NewsContainer.vue
@@ -3,9 +3,27 @@
 </script>
 
 <template>
-    
-
-
+    <div class="container-md w-50 mx-5 p-5 position-relative bottom-50 end-25">
+        <p class="text-black-75 bg-white fs-4 p-4">El fandom de The Witcher no termina de asumir la partida de Henry Cavill tras la
+            tercera
+            temporada, intentando conocer aún cuál ha sido el verdadero motivo. Los desencuentros del británico con la
+            dirección
+            creativa de la serie, como seguidor de los libros originales de Andrzej Sapkowski, quedaba patente mientras el
+            equipo se echaba los trastos a la cabeza. Ahora, uno de los directores de la serie, Marc Jobst, ha añadido una
+            nueva
+            razón por la que Cavill habría podido abandonar la producción de Netflix.
+            <br>
+            <br>
+            "Henry ha hecho tres temporadas que son muy exigentes. Son enormes. Henry hace cada golpe en las escenas de
+            acción y no permite que le echen una mano. Si estás haciendo un primer plano de una mano agarrando una espada,
+            esa es su mano de verdad", ha declarado el director sobre Cavill, quien prefiere protagonizar cada secuencia y
+            no dejar que ningún doble haga este trabajo, como viene siendo habitual en este tipo de ficciones.
+        </p>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped>
+div{
+    opacity: 70%;
+}
+</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,14 +1,17 @@
+<script setup>
+import NewsContainer from '../components/NewsContainer.vue';
+</script>
+
 <template>
-     
+  <NewsContainer />
 </template>
   
   
-  <style>
-  
-  body{
-    background-image: url(../assets/img/2052712.jpg);
-    background-size: cover;
-    background-position: center;
-  }
-  </style>
+<style>
+body {
+  background-image: url(../assets/img/2052712.jpg);
+  background-size: cover;
+  background-position: center;
+}
+</style>
   
